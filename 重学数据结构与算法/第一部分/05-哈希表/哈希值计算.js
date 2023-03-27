1929cf355faafa5427d82a309535a9fd90b20c38
zhangheng <heng.top@qq.com>
2023-03-27 22:48:15 +0800

    feat:完成哈希计算函数

---
+++ 重学数据结构与算法/第一部分/05-哈希表/哈希值计算.js
@@ -0,0 +1,12 @@
+// 字符串哈希值计算
+
+function stringHashCode(str) {
+  let hashCode = 0;
+  for (let i = 0; i < str.length; i++) {
+    const c = str.charCodeAt(i);
+    hashCode = (hashCode << 5) - hashCode + c;
+  }
+  return hashCode;
+}
+
+console.log(stringHashCode("jack"));
