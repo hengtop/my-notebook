a6f4c70a13371cb64787c0536ea75cb61ee79af4
zhangheng <heng.top@qq.com>
2024-01-05 22:51:33 +0800

    feat: 完成图的删除，深度优先搜索，广度优先搜索

--- 重学数据结构与算法/第二部分/图/graph.js
+++ 重学数据结构与算法/第二部分/图/graph.js
@@ -21,7 +21,7 @@ class Edge {
 class Graph {
   constructor() {
     this.vertexs = new Map();
-    this.edges = new Set();
+    this.edges = new Map();
   }
 
   addVertex(v) {
@@ -50,17 +50,18 @@ class Graph {
     const key = this.edgeKey(from, to);
     if (fromVertex.toEdges.has(key)) {
       // 更新权值
-      const fromEdge = fromVertex.get(key);
+      const fromEdge = fromVertex.toEdges.get(key);
       fromEdge.weight = weight;
       // 引用关系所以不用更新两个顶点的
       // const toEdge = toVertex.get(key);
       // toEdge.weight = weight;
     } else {
       const edge = new Edge(from, to, weight);
+      // 保存指向顶点的边
       fromVertex.toEdges.set(key, edge);
       toVertex.fromEdges.set(key, edge);
       // 将边保存起来
-      this.edges.add(edge);
+      this.edges.set(key, edge);
     }
   }
 
@@ -69,41 +70,121 @@ class Graph {
     return from.toString() + to.toString();
   }
 
-  removeVertex() {}
-  removeEdge(from, to) {}
+  removeVertex(v) {
+    const vertex = this.vertexs.get(v);
+    if (vertex == null) return;
+    // 迭代map删除边
+    vertex.toEdges.forEach((edge) => {
+      this.removeEdge(edge.from, edge.to);
+    });
+    vertex.fromEdges.forEach((edge) => {
+      this.removeEdge(edge.from, edge.to);
+    });
+    this.vertexs.delete(v);
+  }
+  removeEdge(from, to) {
+    // 先找到顶点
+
+    let fromVertex = this.vertexs.get(from);
+    if (fromVertex == null) return;
+
+    // 到达点
+    let toVertex = this.vertexs.get(to);
+    if (toVertex == null) return;
+
+    const key = this.edgeKey(from, to);
+    fromVertex.toEdges.delete(key);
+    toVertex.fromEdges.delete(key);
+    this.edges.delete(key);
+  }
 
   edgeSize() {
-    return this.edges.size();
+    return this.edges.size;
   }
   vertexSize() {
-    return this.vertexs.size();
+    return this.vertexs.size;
+  }
+
+  breadthFirstSearch(v, handle) {
+    // 获取顶点
+    const vertex = this.vertexs.get(v);
+    if (vertex == null) return;
+    const finishV = new Map();
+    const queue = [];
+    queue.push(vertex);
+    finishV.set(v, vertex);
+    while (queue.length !== 0) {
+      const shiftV = queue.shift();
+      handle(shiftV);
+      for (const [, value] of shiftV.toEdges) {
+        // 是否已经访问过
+        if (finishV.has(value.to)) continue;
+        // 找到顶点
+        const nextV = this.vertexs.get(value.to);
+        // 入队
+        queue.push(nextV);
+        // 标记访问
+        finishV.set(value.to, nextV);
+      }
+    }
+
+    finishV.clear();
+  }
+
+  depthFirstSearch(v, handle) {
+    const dfs = (v, handle) => {
+      if (finishV.has(v)) return;
+      const vertex = this.vertexs.get(v);
+      if (vertex == null) return;
+      handle(vertex);
+      finishV.set(v, vertex);
+      for (const [, value] of vertex.toEdges) {
+        dfs(value.to, handle);
+      }
+    };
+    const finishV = new Map();
+    dfs(v, handle);
+    finishV.clear();
   }
 }
 
-const g = new Graph();
+// const g = new Graph();
 
-g.addEdge("v1", "v0", 9);
-g.addEdge("v1", "v2", 3);
-g.addEdge("v2", "v0", 2);
-g.addEdge("v2", "v3", 5);
-g.addEdge("v3", "v4", 1);
-g.addEdge("v0", "v4", 6);
+// g.addEdge("v1", "v0", 9);
+// g.addEdge("v1", "v2", 3);
+// g.addEdge("v2", "v0", 2);
+// g.addEdge("v2", "v3", 5);
+// g.addEdge("v3", "v4", 1);
+// g.addEdge("v0", "v4", 6);
+//g.removeEdge("v0", "v4");
+
+//g.removeVertex("v0");
+// g.breadthFirstSearch("v1", (v) => {
+//   console.log(v.value);
+// });
 
 // 所有顶点
 
-let v = "";
-g.vertexs.forEach((item) => {
-  v += "-" + item.value;
-});
-console.log(v);
+// let v = "";
+// g.vertexs.forEach((item) => {
+//   v += "-" + item.value;
+// });
+// console.log(v);
 
-g.vertexs.forEach((item) => {
-  console.log("---" + item.value);
-  console.log("from：");
-  console.log(item.fromEdges);
-  console.log("to:");
-  console.log(item.toEdges);
-  console.log("--------------------------------");
-});
+// g.vertexs.forEach((item) => {
+//   console.log("---" + item.value);
+//   console.log("from：");
+//   console.log(item.fromEdges);
+//   console.log("to:");
+//   console.log(item.toEdges);
+//   console.log("--------------------------------");
+// });
 
-console.log(g.edges);
+// console.log("边", g.edgeSize());
+// console.log("顶点", g.vertexSize());
+
+// console.log(g.edges);
+
+module.exports = {
+  Graph,
+};
---
+++ 重学数据结构与算法/第二部分/图/test.js
@@ -0,0 +1,98 @@
+const { Graph } = require("./graph");
+
+const BFS_01 = [
+  ["A", "B"],
+  ["A", "F"],
+  ["B", "C"],
+  ["B", "I"],
+  ["B", "G"],
+  ["C", "I"],
+  ["C", "D"],
+  ["D", "I"],
+  ["D", "G"],
+  ["D", "E"],
+  ["D", "H"],
+  ["E", "H"],
+  ["E", "F"],
+  ["F", "G"],
+  ["G", "H"],
+];
+
+const BFS_02 = [
+  [0, 1],
+  [0, 4],
+  [1, 2],
+  [2, 0],
+  [2, 4],
+  [2, 5],
+  [3, 1],
+  [4, 6],
+  [4, 7],
+  [5, 3],
+  [5, 7],
+  [6, 2],
+  [6, 7],
+];
+
+const DFS_01 = [
+  [0, 1],
+  [1, 3],
+  [1, 5],
+  [1, 6],
+  [1, 2],
+  [2, 4],
+  [3, 7],
+];
+
+const DFS_02 = [
+  ["a", "b"],
+  ["a", "e"],
+  ["b", "e"],
+  ["c", "b"],
+  ["d", "a"],
+  ["e", "c"],
+  ["e", "f"],
+  ["f", "c"],
+];
+
+// 有向图初始化
+function d_initGraph(g, data) {
+  data.forEach((item) => {
+    g.addEdge(item[0], item[1], item[2]);
+  });
+
+  return g;
+}
+
+// 无向图初始化
+function ud_initGraph(g, data) {
+  data.forEach((item) => {
+    g.addEdge(item[0], item[1], item[2]);
+    g.addEdge(item[1], item[0], item[2]);
+  });
+  return g;
+}
+
+const g1 = ud_initGraph(new Graph(), BFS_01);
+
+// g1.breadthFirstSearch("A", (v) => {
+//   console.log(v.value);
+// });
+
+const g2 = d_initGraph(new Graph(), BFS_02);
+
+// g2.breadthFirstSearch(5, (v) => {
+//   console.log(v.value);
+// });
+
+const g3 = ud_initGraph(new Graph(), DFS_01);
+
+// g3.depthFirstSearch(1, (v) => {
+//   console.log(v.value);
+// });
+
+const g4 = d_initGraph(new Graph(), DFS_02);
+
+g4.depthFirstSearch("a", (v) => {
+  console.log(v.value);
+});
