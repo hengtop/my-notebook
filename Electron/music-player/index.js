ada0c6474d5c983e115df65928381b96225f64fd
zhangheng <heng.top@qq.com>
2025-08-10 15:06:12 +0800

    refactor: 使用amplitudejs改造原生播放器

--- Electron/music-player/index.js
+++ Electron/music-player/index.js
@@ -1,45 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // 获取DOM元素
-  const audioPlayer = document.getElementById("audioPlayer");
-  const playList = document.getElementById("playList");
-  const ListArr = document.querySelectorAll("#playList li");
-  const currentPlayingTrackName = document.querySelector(
-    ".currentPlayingTrackName"
-  );
-  let currentSong = 0;
+// 固定歌曲列表
+const songs = [
+  {
+    name: "Aimer - 凍えそうな季節から",
+    url: "./assets/music/Aimer - 凍えそうな季節から.m4a",
+  },
+  {
+    name: "アトラスサウンドチーム - やすらぎ -Reload-",
+    url: "./assets/music/アトラスサウンドチーム - やすらぎ -Reload-.flac",
+  },
+  {
+    name: "周杰伦 - 七里香",
+    url: "./assets/music/周杰伦 - 七里 香.flac",
+  },
+  {
+    name: "周杰伦 - 反方向的钟",
+    url: "./assets/music/周杰伦 - 反 方向 的钟.flac",
+  },
+  {
+    name: "周杰伦 - 搁浅",
+    url: "./assets/music/周杰伦 - 搁 浅.flac",
+  },
+  {
+    name: "周杰伦 - 龙卷风",
+    url: "./assets/music/周杰伦 - 龙卷 风.flac",
+  },
+  {
+    name: "川村ゆみ - キミの記憶",
+    url: "./assets/music/川村ゆみ - キミの記憶.flac",
+  },
+];
 
-  function setIndexPlayer(index) {
-    const currentLi = ListArr[index];
-    ListArr.forEach((li) => li.classList.remove("playing"));
-    currentLi.classList.add("playing");
-
-    const songPath = currentLi.getAttribute("data-song");
-    audioPlayer.src = songPath;
-    audioPlayer.load(); // 重新加载音频源
-
-    // 更新当前播放显示
-    const songName = currentLi.textContent;
-    currentPlayingTrackName.textContent = songName;
-
-    // 播放音乐
-    audioPlayer.play().catch((e) => console.error("播放失败:", e));
-  }
-
-  // 切换为事件委托形式
-  playList.addEventListener("click", (e) => {
-    if (e.target.nodeName === "LI") {
-      // 获取索引
-      const index = Array.prototype.slice.call(ListArr).indexOf(e.target);
-      setIndexPlayer(index);
-      currentSong = index;
-    }
-  });
-
-  // 初始化显示
-  currentPlayingTrackName.textContent = "无音源播放";
-
-  audioPlayer.addEventListener("ended", () => {
-    currentSong = (currentSong + 1) % ListArr.length;
-    setIndexPlayer(currentSong);
-  });
+Amplitude.init({
+  songs,
 });
